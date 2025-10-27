7b249c9f065ff8a4c456bd27f5bedb6826e65ece
GuanhuaMa <s4833926@student.uq.edu.au>
2025-10-27 16:55:33 +1100

    feat: Implement forward pass and loss calculation

--- recognition/1_UNet_48339261/train.py
+++ recognition/1_UNet_48339261/train.py
@@ -5,12 +5,19 @@ def train(model, train_loader, test_dataset, epochs=3, lr=0.001, visualize_every
     for epoch in range(epochs):
         model.train() 
 
+        criterion = DiceLoss()
+
         for batch_idx, (images, masks) in enumerate(train_loader):
 
             images = images.to(device)
             masks = masks.to(device)
+            
+            # Forward
+            outputs = model(images)
+            pred_pet = outputs[:, 0]  # (C, H, W)
+            loss = criterion(pred_pet, masks)
 
-            # TODO: forward and loss function
+            # Backward
             pass 
 
         print(f"Epoch {epoch+1}/{epochs} Complete")
