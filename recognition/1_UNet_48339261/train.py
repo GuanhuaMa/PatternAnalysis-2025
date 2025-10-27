3a501d391630db278dd2cc4db33cdb20374170d6
GuanhuaMa <s4833926@student.uq.edu.au>
2025-10-27 17:00:07 +1100

    feat: Add optimizer and backpropagation

--- recognition/1_UNet_48339261/train.py
+++ recognition/1_UNet_48339261/train.py
@@ -1,11 +1,14 @@
 def train(model, train_loader, test_dataset, epochs=3, lr=0.001, visualize_every=1):
 
+
+    criterion = DiceLoss()
+
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+
     print(f"Train Starting for {epochs} epochs")
 
     for epoch in range(epochs):
         model.train() 
-
-        criterion = DiceLoss()
 
         for batch_idx, (images, masks) in enumerate(train_loader):
 
@@ -16,8 +19,13 @@ def train(model, train_loader, test_dataset, epochs=3, lr=0.001, visualize_every
             outputs = model(images)
             pred_pet = outputs[:, 0]  # (C, H, W)
             loss = criterion(pred_pet, masks)
+            
+            # Backpropagation
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-            # Backward
+
             pass 
 
         print(f"Epoch {epoch+1}/{epochs} Complete")
