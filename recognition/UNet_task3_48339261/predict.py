a903ede469e4145e7006067e63325a8b973ca589
GuanhuaMa <s4833926@student.uq.edu.au>
2025-10-30 15:36:50 +1100

    fix: change the data path for process in Colab

--- recognition/UNet_task3_48339261/predict.py
+++ recognition/UNet_task3_48339261/predict.py
@@ -59,8 +59,8 @@ def show_predictions(model, dataset, title="Final segmentation results (HipMRI)"
 if __name__ == '__main__':
     print("Runing predict.py...")
 
-    DATA_DIR = "/home/groups/comp3710/HipMRI_Study_open/keras_slices_data"
-    MODEL_SAVE_PATH = "hipmri_unet_model.pth" 
+    DATA_DIR = "/content/drive/MyDrive/Colab-Notebooks/UNet_task3_48339261/keras_slices_data" 
+    MODEL_SAVE_PATH = "/content/drive/MyDrive/Colab-Notebooks/UNet_task3_48339261/hipmri_unet_model.pth"
     RESIZE_TO = (128, 128) 
     PROSTATE_LABEL = 5 
     NUM_EXAMPLES_TO_SHOW = 3
